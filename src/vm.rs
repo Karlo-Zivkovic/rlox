80141a29276c8892b7fbfdd5306dced4f02e26c3
Karlo Zivkovic <karlo.zivkovic2@gmail.com>
2025-01-28 22:07:31 +0100

    feat: opcode functionality for 'print 3 + 3' done

--- src/vm.rs
+++ src/vm.rs
@@ -46,30 +46,51 @@ impl VM {
                 .code
                 .len()
         {
-            match self.advance() {
+            let chunk = self.chunk.as_ref().expect("Chunk should be initialized");
+            let opcode = &chunk.code[self.ip];
+            self.ip += 1;
+
+            match opcode {
                 OpCode::Constant(index) => {
-                    let constant = self.read_constant(index);
-                    self.push(constant);
-                    println!("Constant({})", index);
+                    let constant = chunk.constants[*index as usize].clone();
+                    self.stack.push(constant);
                 }
+                OpCode::Add => {
+                    let b = self.stack.pop().unwrap();
+                    let a = self.stack.pop().unwrap();
+                    match (a, b) {
+                        (Value::Number(a), Value::Number(b)) => {
+                            self.stack.push(Value::Number(a + b));
+                        }
+                        _ => return InterpretResult::RuntimeError,
+                    }
+                }
+                OpCode::Print => {
+                    let value = self.stack.pop().unwrap();
+                    println!("{:#?}", value);
+                }
+                OpCode::Return => return InterpretResult::Ok,
                 _ => println!("other"),
             }
         }
+
+        dbg!(&self.chunk);
         println!("Running VM...");
         InterpretResult::Ok
     }
 
-    fn read_constant(&self, index: &u8) -> Value {
-        self.chunk
-            .as_ref()
-            .expect("Chunk should be initialized")
-            .constants[self.ip]
-    }
+    // fn read_constant(&self, index: &u8) -> Value {
+    //     self.chunk
+    //         .as_ref()
+    //         .expect("Chunk should be initialized")
+    //         .constants[*index as usize]
+    //         .clone()
+    // }
 
-    fn advance(&mut self) -> &OpCode {
-        let chunk = self.chunk.as_ref().expect("Chunk should be initialized");
-        let opcode = &chunk.code[self.ip];
-        self.ip += 1;
-        opcode
-    }
+    // fn advance(&mut self) -> &OpCode {
+    //     let chunk = self.chunk.as_ref().expect("Chunk should be initialized");
+    //     let opcode = &chunk.code[self.ip];
+    //     self.ip += 1;
+    //     opcode
+    // }
 }
